22285d033afdca39d4c1c6044da153a9b6fa70d6
Maximal <iMaximal@users.noreply.github.com>
2018-02-22 14:35:55 +0500

    add apollo client in react app

--- client/index.js
+++ client/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-client'
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id,
+  link: new HttpLink(),
+  cache: new InMemoryCache(),
+})
 
 const Root = () => (
-  <div>
+  <ApolloProvider client={client} >
+    <div>
       Auth Starter
-  </div>
+    </div>
+  </ApolloProvider>
 )
 
 ReactDOM.render(<Root />, document.querySelector('#root'))
