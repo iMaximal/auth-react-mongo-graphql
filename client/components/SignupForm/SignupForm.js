bc0b4bf1adbad2b574bdfe2a559e8577242d993e
Maximal <maxim@infofree.ru>
2018-02-25 22:09:27 +0500

    Handling Errors Around Signup

--- client/components/SignupForm/SignupForm.js
+++ client/components/SignupForm/SignupForm.js
@@ -5,10 +5,25 @@ import Signup from '../../mutations/Signup'
 import CurrentUser from '../../queries/CurrentUser'
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      errors: [],
+    }
+  }
+
   onSubmit = ({ email, password }) => {
     this.props.signup({
       variables: { email, password },
+      refetchQueries: [{ query: CurrentUser }],
     })
+      .then(() => this.props.history.push('/'))
+      .catch((res) => {
+        const errors = res.graphQLErrors.map((error) => error.message)
+
+        this.setState({ errors })
+      })
   }
 
   render() {
@@ -16,7 +31,7 @@ class SignupForm extends Component {
       <div>
         <h3>Sign Up</h3>
         <AuthForm
-          errors={[]}
+          errors={this.state.errors}
           onSubmit={this.onSubmit}
         />
       </div>
